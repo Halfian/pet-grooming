e08ad6a99f7e956ca12e1dacc8ee98e23b3a27e7
Halfian <halfihalfian182@gmail.com>
2026-03-17 16:52:20 +0700

    "Fix hashRouter"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,11 @@ import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
 
+console.log("React app starting...");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      console.log("React app starting...");
       <App />
     </HashRouter>
   </StrictMode>,
