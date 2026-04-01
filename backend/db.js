449449803b7344b5f6f24853fb95cd028eaedd59
Halfian <halfihalfian182@gmail.com>
2026-04-01 16:51:22 +0700

    "fix: Adding .env for development and production"

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,25 @@
 import pkg from "pg";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ 
+    path: process.env.NODE_ENV === "production"
+    ? "./.env.production"
+    : "./.env.development" });
 
 const { Pool } = pkg;
 
-const pool = new Pool({
+const pool = process.env.NODE_ENV === "production"
+? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, // Neon requires SSL
+})
+: new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: String(process.env.PGPASSWORD), // force to string
     port: process.env.PGPORT,
-    ssl: process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false } // Neon requires SSL
-        : false, // local dev disables SSL
+    ssl: false,
 });
 
 export default pool;
