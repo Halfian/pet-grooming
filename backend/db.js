6cde3d0116ebd4c4c65499f74e3c73bc96809dbd
Halfian <halfihalfian182@gmail.com>
2026-08-26 14:17:49 +0700

    "fix: DATABASE_URI simplified"

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,20 @@
 import pkg from "pg";
 import dotenv from "dotenv";
 
-dotenv.config({ 
-    path: process.env.NODE_ENV === "production"
-    ? "./.env.production"
-    : "./.env.development" });
+dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = process.env.NODE_ENV === "production"
+// Check if connection string exist where Vercel will look for it in the environment variables
+const isProduction = process.env.DATABASE_URL ? true : false;
+
+const pool = isProduction
 ? new Pool({
       connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }, // Neon requires SSL
+      ssl: { rejectUnauthorized: false }, // Neon requires SSL also keeps connection secure for cloud hosts
 })
 : new Pool({
+    // Local database configuration fallback
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
